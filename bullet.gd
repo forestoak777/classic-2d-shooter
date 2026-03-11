ab321a59d958b10b061c7cd7735931babf9d2fc2
forestoak777 <forestoak777@gmail.com>
2026-03-11 04:48:52 -0400

    bullets destroy environment

--- bullet.gd
+++ bullet.gd
@@ -1,20 +1,40 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = -250
+
+var explosion_particle = preload("res://explosion_particle.tscn")
 
 func start(pos, rot):
 	position = pos
 	rotation = rot
 	
-func _process(delta):
-	position += Vector2.from_angle(rotation + deg_to_rad(90)) * speed * delta
+func _physics_process(delta):
+	var direction = Vector2.from_angle(rotation + deg_to_rad(90))
+	var motion = direction * speed * delta
+	
+	var collision = move_and_collide(motion)
+	
+	if collision:
+		var collider = collision.get_collider()
+	
+		if collider is TileMapLayer:
+			_destroy_tile(collider, collision.get_position(), collision.get_normal())
+			_spawn_effect(collision.get_position())
+			queue_free()
+		elif collider.is_in_group("enemies"):
+			collider.explode()
+			queue_free()
+			
 
+func _destroy_tile(tilemap: TileMapLayer, hit_pos: Vector2, normal: Vector2):
+	var inset_pos = hit_pos - normal * 1.0
+	var tile_pos = tilemap.local_to_map(tilemap.to_local(inset_pos))
+	tilemap.erase_cell(tile_pos)
+
+func _spawn_effect(pos: Vector2):
+	var effect = explosion_particle.instantiate()
+	get_tree().root.add_child(effect)
+	effect.global_position = pos
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
-
-
-func _on_area_entered(area):
-	if area.is_in_group("enemies"):
-		area.explode()
-		queue_free()
